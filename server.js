ed86f1c2fe9b83bb5d6b73ca4ee65f77adbfab2d
gerrylon <jilong_wan@outlook.com>
2018-03-23 17:33:00 +0800

    jsonp test success

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var argv = require('yargs').argv;
+const url = require('url');
 
 // 默认监听端口
 var port = 8900;
@@ -11,21 +12,33 @@ if (argv.p) {
 
 http.createServer(function(request, response) {
   // response.end('Hello World\n');
-  var url = request.url;
 
-  if (url.indexOf('/api') === 0) {
-    var realApi = url.substr(4);
+  var urlObj = url.parse(request.url, true);
 
-    console.log(realApi);
+  // console.log(urlObj);
+  var pathname = urlObj.pathname;
+
+  if (pathname.indexOf('/api') === 0) {
+    var realApi = pathname.substr(4);
     switch (realApi) {
       case '/get1':
         response.end('get1 success');
         break;
-        
+
       case '/get2':
         response.setHeader('Access-Control-Allow-Origin', '*');
         response.end('get2 success');
         break;
+
+      case '/get3':
+        // response.end('get3 success');
+        response.end(urlObj.query.callback + '(' + '"get3 success"' + ')');
+        break;
+
+      default:
+        console.log(urlObj);
+        response.end(JSON.stringify(urlObj));
+
     }
   }
 }).listen(port)
