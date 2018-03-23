a77a00023bde0d217e8fddc348160d0ce730de05
gerrylon <jilong_wan@outlook.com>
2018-03-23 15:46:02 +0800

    jasmine environment builded success

--- server.js
+++ server.js
@@ -6,12 +6,28 @@ var port = 8900;
 
 // 可设置不同端口
 if (argv.p) {
-  port = argv.p;
+  port = +argv.p;
 }
 
 http.createServer(function(request, response) {
   // response.end('Hello World\n');
+  var url = request.url;
 
+  if (url.indexOf('/api') === 0) {
+    var realApi = url.substr(4);
+
+    console.log(realApi);
+    switch (realApi) {
+      case '/get1':
+        response.end('get1 success');
+        break;
+        
+      case '/get2':
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.end('get2 success');
+        break;
+    }
+  }
 }).listen(port)
 
 console.log('Server running at http://127.0.0.1:' + port);  
